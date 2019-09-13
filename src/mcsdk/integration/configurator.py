6872d17093d7822f46a0c6c0c6fd6a5edb69234b
sfadincescu <adincescu@salesforce.com>
2019-09-13 15:35:13 +0300

    Rebuild

--- src/mcsdk/integration/configurator.py
+++ src/mcsdk/integration/configurator.py
@@ -2,7 +2,7 @@ import os
 import yaml
 
 
-def yaml_import(root_dir):
+def yaml_import(config_dir):
     """ Extracts the configuration from a file """
-    with open(os.path.join(root_dir, 'ci-config.yml'), 'r') as file:
+    with open(os.path.join(config_dir, 'ci-config.yml'), 'r') as file:
         return yaml.safe_load(file)
